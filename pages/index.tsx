0e1a597f2c44236e5b3205fcfe6257fab4f8cb7b
berlysia <berlysia@gmail.com>
2021-12-23 21:55:55 +0900

    add interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,46 @@
-import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
+import Head from "next/head";
+import { useCallback, useReducer, useState } from "react";
+import styles from "../styles/Home.module.css";
 
+function KV({name = "", value = ""}: { name?: string, value?:string}) {
+  const [myName, setMyName] = useState(name);
+  const [myValue, setMyValue] = useState(value);
+  const handleNameChange = useCallback((e) => setMyName(e.currentTarget.value), []);
+  const handleValueChange = useCallback(
+    (e) => setMyValue(e.currentTarget.value),
+    []
+  );
+  return (
+    <div>
+      <label>
+        <input onChange={handleNameChange} value={myName}></input>:
+        <input
+          onChange={handleValueChange}
+          name={myName}
+          value={String(myValue)}
+        />
+      </label>
+    </div>
+  );
+}
 
-
-export const getServerSideProps: GetServerSideProps =  async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     props: {
       query: context.query,
     },
   };
-}
+};
 
 const Home: NextPage = (
   props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) => {
+  const [addedFields, addField] = useReducer(x => ++x, 0);
   return (
     <div className={styles.container}>
       <Head>
@@ -28,19 +53,21 @@ const Home: NextPage = (
 
       <main className={styles.main}>
         <h1>OGPみるやつ</h1>
-        <table>
-          <tbody>
-            {Object.entries(props.query).map(([key, value]) => (
-              <tr key={key}>
-                <th>{key}</th>
-                <td>{String(value)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <form method="get" action="./">
+          {Object.entries(props.query).map(([name, value]) => (
+            <KV key={name} name={name} value={String(value)} />
+          ))}
+          <div>
+            <button type="button" onClick={addField}>
+              add field
+            </button>
+          </div>
+          {Array.from({ length: addedFields }, (x, i) => <KV key={i} />)}
+          <button type="submit">submit</button>
+        </form>
       </main>
     </div>
   );
 };
 
-export default Home
+export default Home;
